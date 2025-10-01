5faa68a41448c3a361727f409937cdc370795aef
asukas00 <jessere2025@gmail.com>
2025-10-01 07:22:20 -0700

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,17 +1,20 @@
 items = [
-    { "name": "Apple"
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
+{     "name": "apple",
+    "price": 0.99,  
+    "department": "fruits"
+}
+{
+    "name": "strawberry",
+    "price": 4.99,
+    "department": "fruits"}
 ]
+
+    
+
+   
+
+
+
+
+
+print(items["name"])
