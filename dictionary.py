189060b080a2372b8ccb37ab077aba96fe026333
asukas00 <jessere2025@gmail.com>
2025-10-16 06:55:05 -0700

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,22 +1,33 @@
 items = [
 {
     "name": "apple",
-    "price" :0.99,  
-    "department": "fruits",
-    },
+    "price": 0.99,
+    "department": "fruits"},
 
 {
     "name": "strawberry",
     "price": 4.99,
     "department": "fruits"},
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
-
-    
+{
+    "name": "banana",
+    "price": 0.50,
+    "department": "fruits"},
+{
+    "name": "cat",
+    "price": 670,
+    "department": "pet"},
+{
+    "name": "dog",
+    "price": 500,
+    "department": "pet"},
+{
+    "name": "airpod",
+    "price": 129,
+    "department": "technology"},
+                                                                                                                                                                                                                                                                                                                                                                 
 ]
-   
 
 
 
 
-
-print(items[0]["price"])
+print(items[0,1,2,3]["price"])
